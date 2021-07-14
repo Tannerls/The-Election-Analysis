9b896f15896994a206d9dcdfcc57d59dadd1a736
Tanner <83738699+Tannerls@users.noreply.github.com>
2021-07-14 14:14:04 -0500

    Adding the Election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -48,31 +48,56 @@ with open(file_to_load) as election_data:
 
         canidate_votes[canidate_name] += 1
 
-    # move through the caidate list
-    for canidate_name in canidate_votes:
-        #retrieve vote count
-        votes = canidate_votes[canidate_name]
 
-        #calculating percentage
-        vote_percentage = float(votes) / float(total_votes) * 100
+    # save results to text file
+    with open(file_to_save, "w") as txt_file:
+        canidate_votes[canidate_name] += 1
 
-        #Print canidate name and percitage
-        print(f"{canidate_name}: {vote_percentage: .2f}% ({votes:,})\n ")
-
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            #if ture then set winning_count = votes and winning_percentage = to vote_percentage
-            winning_count = votes
-            winning_percentage = vote_percentage
-            #and, set the wiining_candidate equal to the canidates name.
-            winning_canidate = canidate_name
-
-
-#print(f"{winning_canidate}: received {winning_count} votes, and won with {winning_percentage: .2f} of the total votes. ")
-        winning_canidate_summary = (
+    # Print the final vote count to the terminal.
+        election_results = (
+            f"\nElection Results\n"
             f"-------------------------\n"
-            f"Winner: {winning_canidate}\n"
-            f"Winning Vote Count: {winning_count:,}\n"
-            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"Total Votes: {total_votes:,}\n"
             f"-------------------------\n")
+        print(election_results, end="")
+        # Save the final vote count to the text file.
+        txt_file.write(election_results)
 
-print(winning_canidate_summary)
+        # move through the caidate list
+        for canidate_name in canidate_votes:
+            #retrieve vote count
+            votes = canidate_votes[canidate_name]
+
+            #calculating percentage
+            vote_percentage = float(votes) / float(total_votes) * 100
+
+            canidate_results = (
+            f"{canidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+             #print(f"{canidate_name}: {vote_percentage: .2f}% ({votes:,})\n ")
+            print(canidate_results)
+           
+            #  Save the candidate results to our text file.
+            txt_file.write(canidate_results)
+
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                #if ture then set winning_count = votes and winning_percentage = to vote_percentage
+                winning_count = votes
+                winning_percentage = vote_percentage
+                #and, set the wiining_candidate equal to the canidates name.
+                winning_canidate = canidate_name
+
+
+    #print(f"{winning_canidate}: received {winning_count} votes, and won with {winning_percentage: .2f} of the total votes. ")
+            winning_canidate_summary = (
+                f"-------------------------\n"
+                f"Winner: {winning_canidate}\n"
+                f"Winning Vote Count: {winning_count:,}\n"
+                f"Winning Percentage: {winning_percentage:.1f}%\n"
+                f"-------------------------\n")
+    #print(winning_canidate_summary)
+        print(winning_canidate_summary)
+        # Save the winning candidate's results to the text file.
+        txt_file.write(winning_canidate_summary)
+            
+            
